1d2989e58beaa0eded738906bbe6c2f3f3813df2
vicentebatista <vicentedepaula@gmail.com>
2014-04-02 21:54:09 +0000

    web.js changed again

--- web.js
+++ web.js
@@ -1,13 +1,18 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+  var buffer = new Buffer(fs.readFileSync('index.html'));
+
+  response.send(buffer.toString());
+
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
-var useless = 1;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
